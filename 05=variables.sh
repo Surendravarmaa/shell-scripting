e4f7782db05554c1c612087da755da039d405d06
unknown <surendravarmajampana.59@gmail.com>
2026-03-26 22:03:53 +0530

    shell-script

---
+++ 05=variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter your username: :"
+
+read -s USERNAME #here USERNAME is variable
+
+echo "Please enter your password: :"
+
+read -s PASSWORD
+
+echo "Username is: $USERNAME, password is $PASSWORD"
