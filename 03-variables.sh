d521ed0c1f3426e4d7231c2570e0c2147f47127d
unknown <surendravarmajampana.59@gmail.com>
2026-03-26 21:44:26 +0530

    shell-script

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-PERSON1=Ramesh #no space between = and value
-PERSON2=Suresh
+#declaring of variable
+PERSON1=Sachin #no space between = and value
+PERSON2=Rahul
 
+#Referring variable
 echo "$PERSON1 : Hello $PERSON2, How are you?"
 echo "$PERSON2 : Hi $PERSON1, I am fine, How are you?"
 echo "$PERSON1 : I am fine too, how is your work?"
