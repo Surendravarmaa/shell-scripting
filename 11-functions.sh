746e589e76274e1219a9f9ef44171d407b3576af
unknown <surendravarmajampana.59@gmail.com>
2026-03-27 18:32:24 +0530

    shell-script

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,8 +3,12 @@
 USERID=$(id -u)
 
 VALIDATE(){
-    echo "Exit status: $1"
-    echo "what are you doing: $2"
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...FAILURE"
+    else
+        echo "$2...SUCCESS"
+    fi
 }
 
 if [ $USERID -ne 0 ]
@@ -20,5 +24,3 @@ VALIDATE $? "Installing MySQL"
 
 dnf install git -y
 VALIDATE $? "Installing git"
-
-echo "is script proceeding?"
