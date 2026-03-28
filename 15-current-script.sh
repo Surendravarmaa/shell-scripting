a061d12f0e7d1730d4940df13649ae47d227a253
unknown <surendravarmajampana.59@gmail.com>
2026-03-28 14:34:13 +0530

    shell-script

---
+++ 15-current-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
++
+COURSE="DevOPs from Current Script"
+
+echo "Before calling other script, course: $COURSE"
+echo "Process ID of current shell script: $$"
+
+# 
+
+echo "After calling other script, course: $COURSE"
